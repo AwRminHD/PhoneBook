ee253aa94de3571cdf92ba8df1ba97da9d2456d5
ArminAzimi <arminhamedazimi2@gmail.com>
2023-04-10 17:39:34 +0330

    Address class constructor upd

--- Address.java
+++ Address.java
@@ -3,6 +3,12 @@ public class Address {
     String country;
     String city;
 
+    public Address(String zipCode, String country, String city) {
+        this.zipCode = zipCode;
+        this.country = country;
+        this.city = city;
+    }
+
     @Override
     public String toString() {
         return "Country: " + country + "\nCity: " + city + "\nZipCode: " + zipCode;
