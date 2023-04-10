7811fe42e365b116287015bff61a02505c2b9a7e
ArminAzimi <arminhamedazimi2@gmail.com>
2023-04-10 17:40:05 +0330

    Contact classs constructor upd

--- Contact.java
+++ Contact.java
@@ -4,4 +4,13 @@ public class Contact {
     
     PhoneNumber phone;
     Address address;
+
+    public Contact(String gp, String email, String Fname, String Lname, PhoneNumber phone, Address address) {
+        this.group = gp;
+        this.email = email;
+        this.firstName = Fname;
+        this.lastName = Lname;
+        this.phone = phone;
+        this.address = address;        
+    }
 }
