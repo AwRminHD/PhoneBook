a21769df4f6852a7699d386f1b4bcf7c340249d5
ArminAzimi <arminhamedazimi2@gmail.com>
2023-04-10 17:40:30 +0330

    PhoneNumber class constructor upd

--- PhoneNumber.java
+++ PhoneNumber.java
@@ -2,6 +2,10 @@ public class PhoneNumber {
     String CountryCode;
     String number;
 
+    public PhoneNumber(String CountryCode, String number) {
+        this.CountryCode = CountryCode;
+        this.number = number;
+    }
     @Override
     public String toString() {
         return CountryCode + " " + number;
